89b1483d38d0e3f7e18470f5f16651647dea3052
ninadbawane-cyber <ninadbawane@gmail.com>
2026-08-10 20:02:57 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+{
+  "name": "origyn-backend",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "dev": "nodemon server.js",
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "dotenv": "^16.0.0",
+    "express": "^4.18.2",
+    "pg": "^8.11.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.0"
+  }
+}
